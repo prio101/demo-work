b18f58adeffd9943cf46757555d07ecc3c8529e8
Mahabub Islam <miprio101@gmail.com>
2024-11-29 16:45:19 +0600

    refactor: remvoed fonts not used

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,6 @@ import localFont from "next/font/local";
 import { useState } from "react";
 import { useEffect } from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
   const handleClick = () => {
